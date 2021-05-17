2c55689e191d7f20fbb641705837c2d74457518d
akez <1791189210@qq.com>
2021-05-17 18:25:43 +0800

    excel

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Exports\InvoicesExport;
-use Maatwebsite\Excel\Facades\Excel;
+use Excel;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Writer\Exception;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ExcelController extends Controller
 {
+    /**
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws Exception
+     */
     public function exportExcel(): BinaryFileResponse
     {
         $data = [
-            ['2021-05-01', 200, 500, 18],
-            ['2021-05-02', 300, 700, 12],
-            ['2021-05-03', 170, 534, 6],
-            ['2021-05-04', 149, 345, 7],
+            ['date' => '2021-05-01', 'area' => "200", 'distance' => "500", 'workTime' => "18"],
+            ['date' => '2021-05-02', 'area' => "456", 'distance' => "788", 'workTime' => "13"],
+            ['date' => '2021-05-03', 'area' => "555", 'distance' => "444", 'workTime' => "22"],
+            ['date' => '2021-05-04', 'area' => "767", 'distance' => "566", 'workTime' => "11"],
         ];
         $headings = [
             ['日期', '面积', '长度', '时长'],
